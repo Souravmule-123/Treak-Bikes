e78fd92698456404c317a8f4084537d3c68de228
Souravmule-123 <souravmule1@gmail.com>
2025-07-19 16:48:06 +0530

    changes in constant file

--- src/main/java/appdata/Constant.java
+++ src/main/java/appdata/Constant.java
@@ -5,5 +5,4 @@ import org.openqa.selenium.By;
 public class Constant {
 
     public static final String PROPERTY_FILE_PATH = "src/test/resources/config/config.properties";
-    public static final By dropDownOptionsList=By.xpath("//*[@name='url']/option");
 }
